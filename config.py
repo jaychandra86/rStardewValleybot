9fe34cf3edeac8916f9dcfa59f244ab9f03f6476
Jay Chandra <58852543+jaypatel69@users.noreply.github.com>
2020-08-01 08:15:54 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,11 @@
+# Twitter consumer keys and access tokens, used for OAuth
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+
+#Reddit api and secret keys
+username = ""
+password = ""
+client_id = ""
+client_secret = ""
